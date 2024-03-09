3d81f486bf7d4228ecc1d8b06bfe20f28a1e27ac
yuyun2000 <15515722313yxw@gmail.com>
2024-03-09 16:06:04 +0800

    修复细节

--- coin1-8.py
+++ coin1-8.py
@@ -87,6 +87,6 @@ while True:
                 append_purchase_log(log_path,symbol,purchase_time, buy_price, sell_price)
             time.sleep(sleeptime)
         except:
-            print('symbol', 'error')
-            traceback.print_exc()
+            print('symbol:',symbol)
+            traceback.print_exc()  # 打印详细的错误信息
             time.sleep(sleeptime)
